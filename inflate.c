faf2bb7509be11f37fd65e18a1c72323d5a30d3c
Rafael Diniz <rafael@riseup.net>
2026-01-21 14:51:06 +0000

    backport bug fix from upstream and increate stack size

--- inflate.c
+++ inflate.c
@@ -234,7 +234,7 @@ int ZEXPORT inflatePrime(z_streamp strm, int bits, int value) {
     }
     if (bits > 16 || state->bits + (uInt)bits > 32) return Z_STREAM_ERROR;
     value &= (1L << bits) - 1;
-    state->hold += (unsigned)value << state->bits;
+    state->hold += (unsigned long)value << state->bits;
     state->bits += (uInt)bits;
     return Z_OK;
 }
